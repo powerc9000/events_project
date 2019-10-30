9e0fdf17069df7d08d9e1d99c1aed0126419f758
powerc9000 <clay.murray8@gmail.com>
2019-10-29 20:35:42 -0600

    update build

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,28 +1,33 @@
-const tailwindcss = require('tailwindcss')
+const tailwindcss = require("tailwindcss");
 const nested = require("postcss-nested");
-const purgecss = require('@fullhuman/postcss-purgecss')({
-
-  // Specify the paths to all of the template files in your project 
-  content: [
-    './templates/**/*.ejs',
-  ],
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./templates/**/*.ejs"],
   whitelist: ["justify-end", "expanded"],
-  whitelistPatternsChildren: [/^token/, /^pre/, /^code/, /^line-numbers/, /^language-json/],
+  whitelistPatternsChildren: [
+    /^token/,
+    /^pre/,
+    /^code/,
+    /^line-numbers/,
+    /^language-json/
+  ],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-})
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
 let purge = [];
 
-if(process.env.NODE_ENV === "production"){
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === "production") {
   console.log("production using purgecss");
   purge = [purgecss];
 }
 module.exports = {
   plugins: [
     nested,
-    tailwindcss('./tailwind.js'),
+    tailwindcss("./tailwind.js"),
     require("autoprefixer"),
     ...purge
   ]
-}
+};
