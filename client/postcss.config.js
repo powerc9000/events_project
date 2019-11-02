715363b37db182d5bcfa63cbb06c1652743c8ec8
powerc9000 <clay.murray8@gmail.com>
2019-11-01 19:05:08 -0600

    Keep images good

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,8 +2,8 @@ const tailwindcss = require("tailwindcss");
 const nested = require("postcss-nested");
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
-  content: ["./templates/**/*.ejs"],
-  whitelist: ["justify-end", "expanded"],
+  content: ["./templates/**/*.ejs", "./templates/**/*.md"],
+  whitelist: ["justify-end", "expanded", "img"],
   whitelistPatternsChildren: [
     /^token/,
     /^pre/,
