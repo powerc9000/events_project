1ab30187f11446791ecba6becfaeb24b6826d3b0
powerc9000 <clay.murray8@gmail.com>
2019-11-08 15:36:02 -0700

    Fix purge

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,7 +2,7 @@ const tailwindcss = require("tailwindcss");
 const nested = require("postcss-nested");
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
-  content: ["./templates/**/*.ejs", "./templates/**/*.md"],
+  content: ["./templates/**/*.njk", "./templates/**/*.md"],
   whitelist: ["justify-end", "expanded", "img"],
   whitelistPatternsChildren: [
     /^token/,
