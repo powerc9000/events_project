3a8c0e1d2858b8c385070210a111071eaa941f3c
powerc9000 <clay.murray8@gmail.com>
2019-11-11 19:03:16 -0700

    Use a date and a time field

--- client/js/controllers/create.js
+++ client/js/controllers/create.js
@@ -1,11 +1,29 @@
 import { ApplicationController } from "../helpers/application_controller";
 import flatpickr from "flatpickr";
+import { format } from "date-fns";
 
 export default class extends ApplicationController {
   connect() {
-    flatpickr(this.targets.find("datepicker"), {
-      enableTime: true,
-      dateFormat: "m/d/Y h:i K"
+    const form = this.targets.find("form");
+    const eventTime = this.data.get("eventDate");
+
+    if (eventTime) {
+      const date = new Date(parseInt(eventTime, 10));
+
+      const day = format(date, "yyyy-LL-dd");
+      const time = format(date, "HH:mm");
+
+      form.date.value = day;
+      form.time.value = time;
+    }
+  }
+  async formInput() {
+    const form = this.targets.find("form");
+
+    [...form.elements].forEach((el) => {
+      if (el.setCustomValidity) {
+        el.setCustomValidity("");
+      }
     });
   }
   async createEvent(e) {
@@ -26,8 +44,22 @@ export default class extends ApplicationController {
       location: form.location.value
     };
 
-    if (form.date.value) {
-      payload.date = new Date(form.date.value).getTime();
+    if (form.date.value || form.time.value) {
+      if (!form.time.value) {
+        this.formControl.error("Time is required when a date is set", "create");
+        form.time.setCustomValidity("Please input a time");
+        return;
+      } else if (!form.date.value) {
+        this.formControl.error("Date is required when a time is set", "create");
+        form.date.setCustomValidity("Please input a date");
+        return;
+      }
+      const date = form.date.value;
+      const time = form.time.value;
+
+      const dateString = `${date}T${time}`;
+
+      payload.date = new Date(dateString).getTime();
     }
 
     if (form.group_id && form.group_id.value) {
@@ -37,8 +69,6 @@ export default class extends ApplicationController {
     if (form.edited_event && form.edited_event.value) {
       extra = `/${form.edited_event.value}`;
     }
-
-    console.log(form.edited_event, extra);
 
     const base = "/api/events";
 
--- client/js/controllers/trix.js
+++ client/js/controllers/trix.js
@@ -1,9 +1,21 @@
 import { ApplicationController } from "../helpers/application_controller";
 
+let loaded = false;
 export default class extends ApplicationController {
   connect() {
+    if (!loaded) {
+      this.loadTrix();
+    }
     this.element.addEventListener("trix-initialize", this.initTrix);
     document.addEventListener("trix-file-accept", this.trixAttach);
+  }
+  loadTrix() {
+    loaded = true;
+    const script = document.createElement("script");
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.0/trix-core.js";
+
+    document.body.appendChild(script);
   }
   initTrix = () => {
     Trix.config.blockAttributes.heading1 = { tagName: "h2" };
