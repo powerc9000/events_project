0790614a695c04fd0a9cb1a29c63638876b09b81
powerc9000 <clay.murray8@gmail.com>
2019-12-02 17:23:53 -0700

    First user event

--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -35,6 +35,19 @@ async function generateLoginToken(userId) {
     }
   };
 
+  //Check first login
+  const check = await server.app.db.maybeOne(
+    sql`select count(*) from logins where user_id = ${userId}`
+  );
+
+  if (check.count === 1) {
+    const user = await server.app.db.maybeOne(
+      sql`select * from users where id=${userId}`
+    );
+    server.createTask("user-first-login", {
+      user
+    });
+  }
   return result;
 }
 
